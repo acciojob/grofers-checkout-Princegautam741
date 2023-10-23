d210fd3a489bf3de0bfed6995f8e2197c7cb7f98
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-10-23 14:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+  // Select all the elements with the class "price"
+  const priceElements = document.querySelectorAll('.price');
+
+  // Initialize the total price
+  let totalPrice = 0;
+
+  // Loop through each price element and add its value to the total price
+  priceElements.forEach((priceElement) => {
+    totalPrice += parseInt(priceElement.textContent, 10);
+  });
+
+  // Create a new row for the total price
+  const table = document.querySelector('table');
+  const newRow = table.insertRow(-1); // Insert at the end of the table
+  newRow.classList.add('total-row');
   
+  // Create a cell in the new row
+  const cell = newRow.insertCell(0);
+  cell.colSpan = 2; // Span both columns
+  cell.textContent = `Total Price: Rs ${totalPrice}`;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
