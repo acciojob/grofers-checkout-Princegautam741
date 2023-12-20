158c16927d05c4ed91a42d02b437b38a6a458257
prince gautam <55788541+Princegautam741@users.noreply.github.com>
2023-12-20 19:18:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,15 +14,23 @@ const getSum = () => {
     totalPrice += parseInt(priceElement.textContent, 10);
   });
 
-  // Create a new row for the total price
-  const table = document.querySelector('table');
-  const newRow = table.insertRow(-1); // Insert at the end of the table
-  newRow.classList.add('total-row');
-  
-  // Create a cell in the new row
-  const cell = newRow.insertCell(0);
-  cell.colSpan = 2; // Span both columns
-  cell.textContent = `Total Price: Rs ${totalPrice}`;
+  // Check if a total row already exists
+  const existingTotalRow = document.querySelector('.total-row');
+
+  if (existingTotalRow) {
+    // Update the content of the existing total row
+    existingTotalRow.cells[0].textContent = `Total Price: Rs ${totalPrice}`;
+  } else {
+    // Create a new row for the total price
+    const table = document.querySelector('table');
+    const newRow = table.insertRow(-1); // Insert at the end of the table
+    newRow.classList.add('total-row');
+
+    // Create a cell in the new row
+    const cell = newRow.insertCell(0);
+    cell.colSpan = 2; // Span both columns
+    cell.textContent = `Total Price: Rs ${totalPrice}`;
+  }
 };
 
 getSumBtn.addEventListener("click", getSum);
